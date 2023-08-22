4da58e36e55043fb969a86cd63480e0879f869b1
ozge_dgn <78146845+ozge-dgn@users.noreply.github.com>
2023-08-22 22:39:30 +0300

    Initial commit

---
+++ src/main/java/com/example/treesbackend/TreesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.treesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/treesbackend/TreesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.treesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
